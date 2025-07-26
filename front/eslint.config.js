152a8d567c4b43b2620f074c039cbff39e46fa5c
Sh1n3zZ <DengJunHai@foxmail.com>
2025-07-26 22:08:21 +0800

    chore: remove TS6133 check

--- front/eslint.config.js
+++ front/eslint.config.js
@@ -23,6 +23,9 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      // Disable unused imports/variables warnings
+      '@typescript-eslint/no-unused-vars': 'off',
+      'no-unused-vars': 'off',
     },
   },
 )
